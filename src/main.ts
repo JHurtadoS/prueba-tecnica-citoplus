510e5237b401395fc476bb64b5b6ad817d5e864d
Juan Esteban Hurtado <jhurtados@car.gov.co>
2024-12-04 23:56:01 -0500

    ajuste cors prueba

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,14 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
 
+  app.enableCors({
+    origin: '*', // Permitir todos los orígenes
+    credentials: true, // Permitir cookies y encabezados de autenticación
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'], // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // Headers permitidos
+  });
+
+
+
 }
 bootstrap();
